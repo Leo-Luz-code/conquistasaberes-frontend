2effadd8565bc78b9be20c6b882076806ede10eb
Poliana Souza <polysantos88@gmail.com>
2026-04-13 13:56:37 -0300

    fix: correct ESLint configuration for unused variables rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -62,6 +62,6 @@ module.exports = {
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "no-unused vars"
+    "no-unused-vars": "off",
   },
 };
